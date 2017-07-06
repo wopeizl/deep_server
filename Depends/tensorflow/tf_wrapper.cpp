067ac6b50589ca9d05fc00ed464857965716c2be
peizhilin <wopeizl@icloud.com>
2017-07-06 17:59:54 +0800

    update

--- Depends/tensorflow/tf_wrapper.cpp
+++ Depends/tensorflow/tf_wrapper.cpp
@@ -8,11 +8,15 @@ namespace tf_wrapper{
         return false;
     }
 
+    bool tf_wrapper::init(string model_name, string model_base_path, string model_config_file){
+        return false;
+    }
+
     bool tf_wrapper::preprocess() {
         return false;
     }
 
-    bool tf_wrapper::predict(vector<unsigned char>& idata, void *p) {
+    bool tf_wrapper::predict(vector<unsigned char>& idata, vector<p_tf_detect_result> & output) {
         return false;
     }
 
